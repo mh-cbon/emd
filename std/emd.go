bce5d60dd940efff8c4fb829ae487df8ba2f37d1
mh-cbon <mh-cbon@users.noreply.github.com>
2017-02-22 21:23:53 +0100

    badge(fix): fix appveyor badge

--- std/emd.go
+++ std/emd.go
@@ -43,7 +43,7 @@ Check the [release page](https://github.com/{{.User}}/{{.Name}}/releases)!
 {{- end}}`)
 
 	g.AddTemplate(`{{define "badge/appveyor" -}}
-[![appveyor Status](https://ci.appveyor.com/api/projects/status/.ProviderName/{{.User}}/{{.Name}}?branch={{.Branch}}&svg=true)](https://ci.appveyor.com/api/projects/status/.ProviderName/{{.User}}/{{.Name}}?branch={{.Branch}}&svg=true)
+[![appveyor Status](https://ci.appveyor.com/api/projects/status/{{.ProviderName}}/{{.User}}/{{.Name}}?branch={{.Branch}}&svg=true)](https://ci.appveyor.com/api/projects/status/{{.ProviderName}}/{{.User}}/{{.Name}}?branch={{.Branch}}&svg=true)
 {{- end}}`)
 
 	g.AddTemplate(`{{define "choco/install" -}}
