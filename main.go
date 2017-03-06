bd7772fc168cdd17b7952583dd2735691bc02004
mh-cbon <mh-cbon@users.noreply.github.com>
2017-03-06 18:09:37 +0100

    doc

--- main.go
+++ main.go
@@ -170,3 +170,29 @@ var defTemplate = `# {{.Name}}
 
 {{goinstall}}
 `
+
+// Example demonstrates the generation
+// of the given README.e.md source file
+// to os.Stdout.
+func Example() {
+
+	// make a new instance of emd.Generator.
+	gen := emd.NewGenerator()
+
+	// set the main template.
+	gen.AddTemplate("{{.Name}}")
+
+	// set the data available in templates.
+	gen.SetDataMap(map[string]interface{}{"Name": "projectname"})
+
+	// register a plugin
+	if err := std.Register(gen); err != nil {
+		panic(err)
+	}
+
+	// process the template.
+	if err := gen.Execute(os.Stdout); err != nil {
+		panic(err)
+	}
+	// Output: projectname
+}
