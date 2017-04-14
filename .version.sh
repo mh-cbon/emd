75a282a1e567377ac2a6dbcaa75dd72de8beaefb
mh-cbon <mh-cbon@users.noreply.github.com>
2017-04-14 15:17:29 +0200

    bump: add utils tests

--- .version.sh
+++ .version.sh
@@ -6,6 +6,7 @@ PREVERSION=
   philea -s "666 go vet %s" "666 go-fmt-fail %s"
   666 go run main.go -version
   666 go test emd/* -v
+  666 go test utils/* -v
   666 changelog finalize --version !newversion!
   666 commit -q -m "changelog: !newversion!" -f change.log
   666 changelog md -o CHANGELOG.md --guess
